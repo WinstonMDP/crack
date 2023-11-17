28b7a6ba3976ee88e5712d8a0b21cbaa1179e402
WinstonMDP <winstonmdp@gmail.com>
2023-11-17 23:01:26 +0300

    sm

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 use std::process::Command;
 
-const DEPENDENCIES_PATH: &str = "tests/dependencies";
+const DEPENDENCIES_PATH: &str = "tests/dependencies/";
 
 /// Clone rolling dependencies in ``<repository_name>.rolling`` directories.
 pub fn clone_rolling_dependencies(repositories: &[&str]) {
@@ -16,26 +16,33 @@ pub fn clone_rolling_dependencies(repositories: &[&str]) {
     }
 }
 
+#[derive(serde::Serialize)]
+pub struct CommitDependency {
+    pub repository: String,
+    pub commit: String,
+}
+
 /// Clone commit dependencies in ``<repository_name>.commit`` directories and
 /// checkout to the respective commits.
-pub fn clone_commit_dependencies(commit_dependencies: &[(&str, &str)]) {
-    for (repository, commit) in commit_dependencies {
-        let dir = repository_name(repository).to_string() + ".commit";
+pub fn clone_commit_dependencies(commit_dependencies: &[CommitDependency]) {
+    for commit_dependency in commit_dependencies {
+        let dir = repository_name(&commit_dependency.repository).to_string() + ".commit";
         let _ = Command::new("git")
             .current_dir(DEPENDENCIES_PATH)
             .arg("clone")
-            .arg(repository)
+            .arg(&commit_dependency.repository)
             .arg(&dir)
             .output();
         let _ = Command::new("git")
             .current_dir(DEPENDENCIES_PATH.to_string() + &dir)
             .arg("checkout")
-            .arg(commit)
+            .arg(&commit_dependency.commit)
             .output();
     }
+    lock(commit_dependencies);
 }
 
-/// ``git_url`` must consists ``.git`` part
+/// ``git_url`` must consist ``.git`` part
 fn repository_name(git_url: &str) -> &str {
     regex::Regex::new(r"/(\w*)\.git")
         .expect(r"'/(\w*)\.git' should be valid")
@@ -44,6 +51,21 @@ fn repository_name(git_url: &str) -> &str {
         .get(1)
         .expect(r"'/(\w*)\.git' should be right to extract a repository name")
         .as_str()
+}
+
+#[derive(serde::Serialize)]
+struct CommitDependencies<'a> {
+    commit_dependencies: &'a [CommitDependency],
+}
+
+fn lock(commit_dependencies: &[CommitDependency]) {
+    let _ = std::fs::write(
+        DEPENDENCIES_PATH.to_string() + "crack.lock",
+        toml::to_string(&CommitDependencies {
+            commit_dependencies,
+        })
+        .expect("fs::write should fall only in very weird situations"),
+    );
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use crack::clone_commit_dependencies;
-
 fn main() {
-    clone_commit_dependencies(&[(
-        "https://github.com/WinstonMDP/WinstonMDP.git",
-        "97f65b91f9748ad6141774905ce224e2cc5469f2",
-    )]);
+    // let some = [CommitDependency {
+    //     repository: "https://github.com/WinstonMDP/WinstonMDP.git".to_string(),
+    //     commit: "97f65b91f9748ad6141774905ce224e2cc5469f2".to_string(),
+    // }];
 }
