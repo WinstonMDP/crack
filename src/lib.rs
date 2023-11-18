c74bbdcc2b5f2cd2a28ce3ca7d418bb002cf1b6f
WinstonMDP <winstonmdp@gmail.com>
2023-11-18 21:51:11 +0300

    sm

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,105 @@
 #![warn(clippy::pedantic)]
 
-use std::process::Command;
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::fmt::Debug;
+use std::{fs, process::Command};
 
 const DEPENDENCIES_PATH: &str = "tests/dependencies/";
 
-/// Clone rolling dependencies in ``<repository_name>.rolling`` directories.
-pub fn clone_rolling_dependencies(repositories: &[&str]) {
-    for repository in repositories {
-        let _ = Command::new("git")
-            .current_dir(DEPENDENCIES_PATH)
-            .arg("clone")
-            .arg(repository)
-            .arg(repository_name(repository).to_string() + ".rolling")
-            .output();
-    }
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Config {
+    pub name: String,
+    pub dependencies: Dependencies,
 }
 
-#[derive(serde::Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Dependencies {
+    pub rolling: Vec<String>,
+    pub commit: Vec<CommitDependency>,
+}
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
 pub struct CommitDependency {
     pub repository: String,
     pub commit: String,
 }
 
+/// Clone rolling dependencies in ``<repository_name>.rolling`` directories.
 /// Clone commit dependencies in ``<repository_name>.commit`` directories and
-/// checkout to the respective commits.
-pub fn clone_commit_dependencies(commit_dependencies: &[CommitDependency]) {
-    for commit_dependency in commit_dependencies {
-        let dir = repository_name(&commit_dependency.repository).to_string() + ".commit";
+/// checkout to the respective commits. Clone only those repositories,
+/// that are not in the lock file.
+pub fn install(config_dependencies: &Dependencies, locked_dependencies: &Dependencies) {
+    for config_rolling_dependency in &config_dependencies.rolling {
+        if !locked_dependencies
+            .rolling
+            .contains(config_rolling_dependency)
+        {
+            let _ = Command::new("git")
+                .current_dir(DEPENDENCIES_PATH)
+                .arg("clone")
+                .arg(config_rolling_dependency)
+                .arg(repository_name(config_rolling_dependency).to_string() + ".rolling")
+                .output();
+        }
+    }
+    for config_commit_dependency in &config_dependencies.commit {
+        if !locked_dependencies
+            .commit
+            .contains(config_commit_dependency)
+        {
+            let dir = repository_name(&config_commit_dependency.repository).to_string() + ".commit";
+            let _ = Command::new("git")
+                .current_dir(DEPENDENCIES_PATH)
+                .arg("clone")
+                .arg(&config_commit_dependency.repository)
+                .arg(&dir)
+                .output();
+            let _ = Command::new("git")
+                .current_dir(DEPENDENCIES_PATH.to_string() + &dir)
+                .arg("checkout")
+                .arg(&config_commit_dependency.commit)
+                .output();
+        }
+    }
+    lock(config_dependencies);
+}
+
+/// Rolling dependencies are only updated.
+pub fn update(locked_rolling_dependencies: &[&str]) {
+    for dependency in locked_rolling_dependencies {
         let _ = Command::new("git")
-            .current_dir(DEPENDENCIES_PATH)
-            .arg("clone")
-            .arg(&commit_dependency.repository)
-            .arg(&dir)
-            .output();
-        let _ = Command::new("git")
-            .current_dir(DEPENDENCIES_PATH.to_string() + &dir)
-            .arg("checkout")
-            .arg(&commit_dependency.commit)
+            .current_dir(DEPENDENCIES_PATH.to_string() + repository_name(dependency) + ".rolling")
+            .arg("pull")
             .output();
     }
-    lock(commit_dependencies);
+}
+
+/// Delete directories, that are not in config file.
+///
+/// # Panics
+///
+/// Impossible
+pub fn clean(config_dependencies: &Dependencies) {
+    let repository_names: Vec<&String> = config_dependencies
+        .rolling
+        .iter()
+        .chain(config_dependencies.commit.iter().map(|x| &x.repository))
+        .collect();
+    for file in fs::read_dir(DEPENDENCIES_PATH).expect("The directory for project should exist") {
+        let dir_name = file.unwrap().file_name().to_str().unwrap().to_string();
+        if !repository_names
+            .iter()
+            .any(|x| Regex::new(repository_name(x)).unwrap().is_match(&dir_name))
+        {
+            let _ = fs::remove_dir_all(dir_name);
+        }
+    }
 }
 
 /// ``git_url`` must consist ``.git`` part
 fn repository_name(git_url: &str) -> &str {
-    regex::Regex::new(r"/(\w*)\.git")
+    Regex::new(r"/(\w*)\.git")
         .expect(r"'/(\w*)\.git' should be valid")
         .captures(git_url)
         .expect(r"'/(\w*)\.git' should be right to extract a repository name")
@@ -53,18 +108,10 @@ fn repository_name(git_url: &str) -> &str {
         .as_str()
 }
 
-#[derive(serde::Serialize)]
-struct CommitDependencies<'a> {
-    commit_dependencies: &'a [CommitDependency],
-}
-
-fn lock(commit_dependencies: &[CommitDependency]) {
-    let _ = std::fs::write(
+fn lock(dependencies: &Dependencies) {
+    let _ = fs::write(
         DEPENDENCIES_PATH.to_string() + "crack.lock",
-        toml::to_string(&CommitDependencies {
-            commit_dependencies,
-        })
-        .expect("fs::write should fall only in very weird situations"),
+        toml::to_string(dependencies).expect("fs::write should fall only in very weird situations"),
     );
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 fn main() {
-    // let some = [CommitDependency {
-    //     repository: "https://github.com/WinstonMDP/WinstonMDP.git".to_string(),
-    //     commit: "97f65b91f9748ad6141774905ce224e2cc5469f2".to_string(),
-    // }];
+    let config: crack::Config = toml::from_str(
+        r#"
+name = "package_name"
+dependencies.rolling = [
+    "https://github.com/monkeyjunglejuice/matrix-emacs-theme.git",
+    "https://github.com/agda/agda.git"
+]
+
+[[dependencies.commit]]
+repository = "https://github.com/jesseleite/nvim-noirbuddy"
+commit = "7d92fc64ae4c23213fd06f0464a72de45887b0ba"
+                                         "#,
+    )
+    .unwrap();
+    println!("{:#?}", config);
+    println!("{:#?}", toml::to_string(&config));
 }
